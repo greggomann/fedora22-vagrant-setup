bc4d942c76acb0283ca7288bb927b8ace6aa97b8
Greg Mann <gregorywmann@gmail.com>
2015-09-12 16:51:36 -0700

    Added automake package install.

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 sudo dnf groupinstall -y "Development Tools"
 
 # Install other Mesos dependencies.
-sudo dnf install -y autoconf maven python-devel java-1.8.0-openjdk-devel zlib-devel libcurl-devel openssl-devel cyrus-sasl-devel cyrus-sasl-md5 apr-devel subversion-devel apr-util-devel
+sudo dnf install -y automake autoconf maven python-devel java-1.8.0-openjdk-devel zlib-devel libcurl-devel openssl-devel cyrus-sasl-devel cyrus-sasl-md5 apr-devel subversion-devel apr-util-devel
