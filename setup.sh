7590e0f03a45a1a2f6766ca6583ac6be74655be1
Greg Mann <gregorywmann@gmail.com>
2015-09-12 19:07:50 -0700

    Removed devel tools install, added git and patch.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,2 @@
-# Install essential development tools.
-sudo dnf groupinstall -y "Development Tools"
-
 # Install other Mesos dependencies.
-sudo dnf install -y automake autoconf libtool maven python-devel java-1.8.0-openjdk-devel zlib-devel libcurl-devel openssl-devel cyrus-sasl-devel cyrus-sasl-md5 apr-devel subversion-devel apr-util-devel
+sudo dnf install -y automake autoconf libtool git maven patch python-devel java-1.8.0-openjdk-devel zlib-devel libcurl-devel openssl-devel cyrus-sasl-devel cyrus-sasl-md5 apr-devel subversion-devel apr-util-devel
