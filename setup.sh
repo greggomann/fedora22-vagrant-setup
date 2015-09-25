6a6e6d1cacae393bcc5bf8647066964fba1998a5
Greg Mann <gregorywmann@gmail.com>
2015-09-25 09:55:58 -0700

    Added Docker installation.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,10 @@
+dnf update -y
+
 # Install other Mesos dependencies.
-sudo dnf install -y gcc gcc-c++ automake autoconf libtool libevent libevent-devel git maven patch python-devel java-1.8.0-openjdk-devel zlib-devel libcurl-devel openssl-devel cyrus-sasl-devel cyrus-sasl-md5 apr-devel subversion-devel apr-util-devel
+dnf install -y gcc gcc-c++ automake autoconf libtool libevent libevent-devel git maven patch python-devel java-1.8.0-openjdk-devel zlib-devel libcurl-devel openssl-devel cyrus-sasl-devel cyrus-sasl-md5 apr-devel subversion-devel apr-util-devel
+
+# install docker from docker website and start docker service.
+command -v docker >/dev/null 2>&1 || { echo 'Will install docker' ; (wget -qO- https://get.docker.com/ | sh) ; }
+usermod -aG docker vagrant
+service docker restart
+
